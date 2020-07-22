bbadea49977ee1e84b45a976cfad7a899e376cae
mhatiboglu <mh1741mh@gmail.com>
2020-07-22 10:57:16 +0300

    Define actions for buttons handler

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,30 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      ...state,
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DECREMENT") {
+    return {
+      ...state,
+      counter: state.counter - 1,
+    };
+  }
+  if (action.type === "ADD") {
+    return {
+      ...state,
+      counter: state.counter + action.value,
+    };
+  }
+  if (action.type === "SUB") {
+    return {
+      ...state,
+      counter: state.counter - action.value,
+    };
+  }
   return state;
 };
 
